58cff5f608a7ed6986b175079a9348137d1100d1
Andrey <prog.qt@gmail.com>
2020-11-14 14:45:44 +0300

    создал основную и вспомогательные таблицы для яблок

---
+++ console/migrations/m201114_103018_create_apple_table.php
@@ -0,0 +1,73 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%apple}}`.
+ */
+class m201114_103018_create_apple_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%apple_color}}', [
+            'id' => $this->primaryKey()->unsigned(),
+            'color' => $this->string(),
+        ]);
+
+        $this->createTable('{{%apple_state}}', [
+            'id' => $this->primaryKey()->unsigned(),
+            'state' => $this->string(),
+        ]);
+
+        $this->createTable('{{%apple}}', [
+            'id' => $this->primaryKey()->unsigned(),
+            'color_id' => $this->integer()->unsigned(),
+            'state_id' => $this->integer()->unsigned(),
+            'size' => $this->decimal(3, 2),
+            'appeared_at' => $this->timestamp(),
+            'fall_at' => $this->timestamp(),
+            'created_at' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at' => 'timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP',
+        ]);
+
+        $this->addForeignKey('fk_apple__color_id', '{{%apple}}', 'color_id', '{{%apple_color}}', 'id');
+        $this->addForeignKey('fk_apple__state_id', '{{%apple}}', 'state_id', '{{%apple_state}}', 'id');
+
+        $this->fillTableAppleState();
+        $this->fillTableAppleColor();
+    }
+
+    private function fillTableAppleState()
+    {
+        $this->batchInsert('{{%apple_state}}', ['state'], [
+            ['state' => 'висит на дереве'],
+            ['state' => 'упало/лежит на земле'],
+            ['state' => 'гнилое яблоко'],
+        ]);
+    }
+
+    private function fillTableAppleColor()
+    {
+        $this->batchInsert('{{%apple_color}}', ['color'], [
+            ['color' => 'green'],
+            ['color' => 'red'],
+            ['color' => 'yellow'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_apple__state_id', '{{%apple}}');
+        $this->dropForeignKey('fk_apple__color_id', '{{%apple}}');
+
+        $this->dropTable('{{%apple}}');
+        $this->dropTable('{{%apple_state}}');
+        $this->dropTable('{{%apple_color}}');
+    }
+}
